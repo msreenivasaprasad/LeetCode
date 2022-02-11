5544262f62e0032bf19b2f5bd92edd2d98d8d48d
msreenivasaprasad <msreenivasaprasad@gmail.com>
2022-02-11 09:55:03 +0530

    CP

--- LeetCode/src/App.java
+++ LeetCode/src/App.java
@@ -3,38 +3,18 @@ import java.util.Map;
 
 public class App {
 	public static void main(String[] args) {
-		System.out.println(romanToInt("LVIII"));
+		System.out.println(largestOddNumber("35427"));
 	}
 
-	public static  int romanToInt(String s) {
-		
-		Map<Character, Integer> data = new HashMap<Character, Integer>();
-		data.put('I', 1);
-		data.put('V', 5);
-		data.put('X', 10);
-		data.put('L', 50);
-		data.put('C', 100);
-		data.put('D', 500);
-		data.put('M', 1000);
-		char[] c = s.toCharArray();
-		int res = 0;
-		for (int i = 0; i < c.length; i++) {
-			if (c[i] == 'I' && i + 1 < c.length && c[i + 1] == 'V') {
-				res += 4;i++;
-			} else if (c[i] == 'I' && i + 1 < c.length && c[i + 1] == 'X') {
-				res += 9;i++;
-			} else if (c[i] == 'X' && i + 1 < c.length && c[i + 1] == 'L') {
-				res += 40;i++;
-			} else if (c[i] == 'X' && i + 1 < c.length && c[i + 1] == 'C') {
-				res += 90;i++;
-			} else if (c[i] == 'C' && i + 1 < c.length && c[i + 1] == 'D') {
-				res += 400;i++;
-			} else if (c[i] == 'C' && i + 1 < c.length && c[i + 1] == 'M') {
-				res += 900;i++;
-			} else {
-				res += data.get(c[i]);
-			}
-		}
-		return res;
-	}
+    public static  String largestOddNumber(String num) {
+        char [] c= num.toCharArray();
+        String res=new String();
+        for(int i=c.length-1;i>=0;i--) {
+        	if(c[i]=='1'||c[i]=='3'||c[i]=='5'||c[i]=='7'||c[i]=='9') {
+        		res=num.substring(0, i+1);
+        		return res;
+        	}
+        }
+        return res;
+     }
 }
