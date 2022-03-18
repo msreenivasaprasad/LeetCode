d05c4e4d7642afabcd57fb4f5caf74d52f857baa
msreenivasaprasad <msreenivasaprasad@gmail.com>
2022-03-18 09:37:38 +0530

    CP

---
+++ LeetCode/src/AR_Slowest_Key.java
@@ -0,0 +1,22 @@
+public class AR_Slowest_Key {
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		int[] num = { 9, 29, 49, 50 };
+		System.out.println(slowestKey(num, "cbcd"));
+	}
+
+	public static char slowestKey(int[] releaseTimes, String keysPressed) {
+		int num = 0;
+		char res = keysPressed.charAt(0);
+		int max = 0;
+		for (int i = 0; i < releaseTimes.length; i++) {
+			if (releaseTimes[i] - num > max || (releaseTimes[i] - num == max && keysPressed.charAt(i) > res)) {
+				max = releaseTimes[i] - num;
+				res = keysPressed.charAt(i);
+			}
+			num = releaseTimes[i];
+		}
+		System.out.println(max);
+		return res;
+	}
+}
